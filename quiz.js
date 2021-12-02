804450d299315d6765ab4ff0e7d0f17bbfe55240
Tanish1111 <80509943+Tanish1111@users.noreply.github.com>
2021-12-02 12:42:03 +0530

    carewell

---
+++ quiz.js
@@ -0,0 +1,21 @@
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+
+Quiz.prototype.ge
+Quiz.prototype.guesstionIndex().isCorrectAnswer(answer)) {
+        this.score++;tQuestionIndex = function() {
+    return th = function(answer) {
+    if(this.getQuesis.questions[this.questionIndex];
+}
+    }
+
+
+    this.questionIndex++;
+}
+
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
